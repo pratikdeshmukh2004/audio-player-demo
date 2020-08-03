e9384b6e714021816495941c7823e28ddb65f920
Pratik <pratikdeshmukhlobhi@gmail.com>
2020-08-03 15:17:54 +0530

    Updates

---
+++ precache-manifest.ab027e9f2b8ac0db2e039c3e9053bba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e28a6d9fa0568064a59d0c4c99affa",
+    "url": "/audio-player-demo/index.html"
+  },
+  {
+    "revision": "7ab98c53843cef35d414",
+    "url": "/audio-player-demo/static/css/main.f35b6b1f.chunk.css"
+  },
+  {
+    "revision": "abcd066691c89e7b1723",
+    "url": "/audio-player-demo/static/js/2.92592528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/audio-player-demo/static/js/2.92592528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab98c53843cef35d414",
+    "url": "/audio-player-demo/static/js/main.6f7a6dcd.chunk.js"
+  },
+  {
+    "revision": "4b82c0e1907826acebf4",
+    "url": "/audio-player-demo/static/js/runtime-main.8a92c833.js"
+  }
+]);
